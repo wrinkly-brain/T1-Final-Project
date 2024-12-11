b234457f06683573f50182c2afc6293b3a280d85
Jonas Mast <jonas.mast@atlasschool.com>
2024-12-11 17:01:18 -0600

    Added latestButtonInfo object, clicking on one of the rover buttons now stores the button's info in said object

--- assets/js/jonasScript.js
+++ assets/js/jonasScript.js
@@ -1,8 +1,18 @@
+// Store button info to use in functions
+const latestButtonInfo = {
+    buttonName: "",
+    buttonClass: "",
+}
+
 const RoverButtons = document.querySelectorAll(".RoverButtons");
 
+// Listen for a click of a rover button to store that button's information in the latestButtonInfo object
 RoverButtons.forEach(button => {
     button.addEventListener("click", (event) => {
         const response = event;
-        console.log(response.target.innerHTML)
+
+        // Take the name of the button and the class and store it in the latestButtonInfo object
+        latestButtonInfo.buttonName = response.target.innerHTML;
+        latestButtonInfo.buttonClass = response.target.className;
     }
 )});
