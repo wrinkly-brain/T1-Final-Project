9c2c19b868d92e4ecd4ea9b77e6ebfd1556595a6
Jonas Mast <jonas.mast@atlasschool.com>
2024-12-11 17:11:50 -0600

    Added event listeners for camera buttons, the event listeners now return the IDs of the button instead of the html content of the button

--- assets/js/jonasScript.js
+++ assets/js/jonasScript.js
@@ -1,18 +1,34 @@
 // Store button info to use in functions
 const latestButtonInfo = {
-    buttonName: "",
+    buttonID: "",
     buttonClass: "",
 }
 
 const RoverButtons = document.querySelectorAll(".RoverButtons");
+const CameraButtons = document.querySelectorAll(".CameraButtons");
 
 // Listen for a click of a rover button to store that button's information in the latestButtonInfo object
 RoverButtons.forEach(button => {
     button.addEventListener("click", (event) => {
         const response = event;
 
-        // Take the name of the button and the class and store it in the latestButtonInfo object
-        latestButtonInfo.buttonName = response.target.innerHTML;
+        // Take the id of the button and the class and store it in the latestButtonInfo object
+        latestButtonInfo.buttonID = response.target.id;
         latestButtonInfo.buttonClass = response.target.className;
+
+        console.log(latestButtonInfo)
+    }
+)});
+
+// Same function as the RoverButton listener, but it's for the camera buttons
+CameraButtons.forEach(button => {
+    button.addEventListener("click", (event) => {
+        const response = event;
+
+        // Take the id of the button and the class and store it in the latestButtonInfo object
+        latestButtonInfo.buttonID = response.target.id;
+        latestButtonInfo.buttonClass = response.target.className;
+
+        console.log(latestButtonInfo);
     }
 )});
