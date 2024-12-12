54c5f6d336ca623350462318090abcb25898764a
Jonas Mast <jonas.mast@atlasschool.com>
2024-12-12 00:36:25 -0600

    I got into the depths of coding and coded a lot. The sol input value is now accepted, the buttons now send information to the correct object, displayImage alters image alt text, and three functions were added. One function clears the photo array, another checks whether images have been put in the photo array, and the final one clears the image and error text. This is a very big commit, sorry.

--- assets/js/jonasScript.js
+++ assets/js/jonasScript.js
@@ -17,49 +17,68 @@ const uriLinkSegments = {
 const RoverButtons = document.querySelectorAll(".RoverButtons");
 const CameraButtons = document.querySelectorAll(".CameraButtons");
 
-// Listen for a click of a rover button to store that button's information in the latestButtonInfo object
+// Listen for a click of a rover button to store that button's information in the tempInfo object
 RoverButtons.forEach(button => {
     button.addEventListener("click", async (event) => {
         const response = event;
 
-        // Take the id of the button and the class and store it in the latestButtonInfo object
-        latestButtonInfo.buttonID = response.target.id;
-        latestButtonInfo.buttonClass = response.target.className;
+        // Take the id of the button and the class and store it in the tempInfo object
+        tempInfo.buttonID = response.target.id;
+        tempInfo.buttonClass = response.target.className;
 
         uriLinkSegments.rover = response.target.id.toLowerCase();
-    
+
         await getMaxSol();
     }
-)});
+    )
+});
 
 // Same function as the RoverButton listener, but it's for the camera buttons
 CameraButtons.forEach(button => {
     button.addEventListener("click", (event) => {
         const response = event;
 
-        // Take the id of the button and the class and store it in the latestButtonInfo object
-        latestButtonInfo.buttonID = response.target.id;
-        latestButtonInfo.buttonClass = response.target.className;
+        // Take the id of the button and the class and store it in the tempInfo object
+        tempInfo.buttonID = response.target.id;
+        tempInfo.buttonClass = response.target.className;
 
         uriLinkSegments.cameraAngle = response.target.id;
 
         console.log(uriLinkSegments);
     }
-)});
+    )
+});
 
 document.getElementById("ApplyChanges").onclick = async () => {
-    const userInputSol = document.getElementById("SolNum");
+    const userInputSol = document.getElementById("SolNum").value;
+
+    console.log(tempInfo.maxSol);
+
+    console.log(Number(userInputSol));
+
+    console.log((Number(userInputSol) >= 0) && (Number(userInputSol) < (tempInfo.maxSol - 1)));
 
     // maxSol is subtracted by 1 to make sure there are uploaded photos
-    if ((userInputSol >= 0) || (userInputSol < (tempInfo.maxSol - 1))) {
-        uriLinkSegments.solNum = userInputSol;
+    if ((userInputSol >= 0) && (userInputSol < (tempInfo.maxSol - 1))) {
+        uriLinkSegments.solNum = Number(userInputSol);
+        console.log("uri link seg: " + uriLinkSegments.solNum);
     }
     else {
         uriLinkSegments.solNum = 0;
     }
 
-    await getRoverImages();
-    displayImage(1);
+    clearPhotoArray();
+    clearPhotoAndText();
+
+    await getRoverImages()
+
+    const photoExistence = checkForPhotos();
+    if (photoExistence == true) {
+        displayImage(1);
+    }
+    else {
+        return;
+    }
 }
 
 async function getMaxSol() {
@@ -87,10 +106,14 @@ async function getRoverImages() {
     try {
         const roverPhotoResponse = await axios.get(`https://api.nasa.gov/mars-photos/api/v1/rovers/${chosenRover}/photos?api_key=${apiKey}&camera=${chosenCamera}&sol=${solNum}`);
 
-        roverPhotoResponse.photos.forEach(photo => {
+        console.log(`https://api.nasa.gov/mars-photos/api/v1/rovers/${chosenRover}/photos?api_key=${apiKey}&camera=${chosenCamera}&sol=${solNum}`);
+        console.log(roverPhotoResponse);
+
+        roverPhotoResponse.data.photos.forEach(photo => {
             photoArray.push(photo.img_src);
         })
 
+        console.log(photoArray);
         return photoArray;
     } catch (error) {
         console.log(error)
@@ -100,5 +123,29 @@ async function getRoverImages() {
 
 function displayImage(index) {
     document.getElementById("RoverImage").setAttribute("src", `${photoArray[index]}`);
-    document.getElementById("RoverImage").setAttribute("alt", `An image from the ${uriLinkSegments.rover.charAt(0).toUpperCase()} rover.`);
+
+    const roverName = uriLinkSegments.rover;
+    const capitalizedRoverName = roverName.charAt(0).toUpperCase() + roverName.slice(1);
+    document.getElementById("RoverImage").setAttribute("alt", `An image from the ${capitalizedRoverName} rover.`);
+}
+
+function clearPhotoArray() {
+    if (photoArray.length > 0) {
+        photoArray = [];
+    }
+}
+
+function checkForPhotos() {
+    if (photoArray.length == 0) {
+        document.getElementById("ImageErrorText").textContent = "No images can be found using those settings. \n Try changing the camera view or solar day value.";
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+
+function clearPhotoAndText() {
+    document.getElementById("RoverImage").setAttribute("src","");
+    document.getElementById("ImageErrorText").textContent = "";
 }
