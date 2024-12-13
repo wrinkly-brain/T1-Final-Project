af652b42eea8c87af2ded2af4cbca2a6df28ee54
Nailah <nailah.markham@atlasschool.com>
2024-12-12 19:51:52 -0600

    trying to fix potential added branch

--- assets/js/nailahScript.js
+++ assets/js/nailahScript.js
@@ -1,4 +1,3 @@
 fetch("https://api.nasa.gov/planetary/apod?api_key=xAP1HKs1wN8evwCa3vQjye47l8WBGVtcM5UFtTT8")
     .then(response => console.log(response))
     .catch(error => console.error(error));
-
