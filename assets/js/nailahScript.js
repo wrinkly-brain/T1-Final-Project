f501fbd6803ac7dc7f3660ddc41731b4eab1a84f
Nailah <nailah.markham@atlasschool.com>
2024-12-13 00:54:01 -0600

    more attempts at figuring out the API

--- assets/js/nailahScript.js
+++ assets/js/nailahScript.js
@@ -1,3 +1,44 @@
-fetch("https://api.nasa.gov/planetary/apod?api_key=xAP1HKs1wN8evwCa3vQjye47l8WBGVtcM5UFtTT8")
-    .then(response => console.log(response))
-    .catch(error => console.error(error));
+// window.onload = getCertainStuffFromAPI();
+
+// async function getCertainStuffFromAPI() {
+//     try {
+//         const response = await axios.get("https://api.nasa.gov/planetary/apod?api_key=xAP1HKs1wN8evwCa3vQjye47l8WBGVtcM5UFtTT8&date=2024-12-12");
+
+//         console.log(response);
+
+//         console.log(response.copywrite);
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
+
+
+// // Specify the API endpoint for user data
+// const apiUrl = 'https://api.nasa.gov/planetary/apod?api_key=xAP1HKs1wN8evwCa3vQjye47l8WBGVtcM5UFtTT8&date=2024-12-12';
+
+// // Make a GET request using the Fetch API
+// fetch(apiUrl)
+//   .then(response => {
+//     if (!response.ok) {
+//       throw new Error('Network response was not ok');
+//     }
+//     return response.json();
+//   })
+//   .then(userData => {
+//     // Process the retrieved user data
+//     console.log('User Data:', userData);
+//   })
+//   .catch(error => {
+//     console.error('Error:', error);
+//   });
+
+axios
+    .get(
+        "https://api.nasa.gov/planetary/apod?api_key=xAP1HKs1wN8evwCa3vQjye47l8WBGVtcM5UFtTT8&date=2024-12-12"
+    )
+    .then(function (response) {
+        console.log(date);
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
