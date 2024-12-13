4ddf552848bc44c7ec0cd4e4154565ee91184288
Nailah <nailah.markham@atlasschool.com>
2024-12-12 22:28:25 -0600

    fixing my wrongs

--- assets/js/nailahScript.js
+++ assets/js/nailahScript.js
@@ -1,13 +1,3 @@
-fetch("https://api.nasa.gov/planetary/apod?api_key=xAP1HKs1wN8evwCa3vQjye47l8WBGVtcM5UFtTT8&date")
+fetch("https://api.nasa.gov/planetary/apod?api_key=xAP1HKs1wN8evwCa3vQjye47l8WBGVtcM5UFtTT8")
     .then(response => console.log(response))
     .catch(error => console.error(error));
-
-// req.addEventListener("load", function(){
-//     if(req.status == 200){
-//         let response = JSON.parse(req.responseText);
-//         document.getElementById("title").textContent = response.title;
-//         document.getElementById("date").textContent = response.date;
-//         document.getElementById("picture").src = response.hdurl;
-//         document.getElementById("explanation").textContent = response.explanation;
-//     }
-// })
