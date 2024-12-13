f5eeb48d3b8b0c0f76493988562d4882da92e7ff
Nailah <nailah.markham@atlasschool.com>
2024-12-13 09:40:55 -0600

    fixed fetchNASA() with added function

--- assets/js/nailahScript.js
+++ assets/js/nailahScript.js
@@ -1,28 +1,20 @@
 
 $(document).ready(function (){
-    fetchNasa()
-    randButton()
+    fetchCurrent()
 });
-
-// $(document).ready(function(){
-//     randButton()
-// });
-
-function randButton(){
-
+document.getElementById("randButton").onclick = () => {
+    fetchNasa();
 }
 
-function fetchNasa(){
+function fetchCurrent(){
 
     $.ajax({
-        url: "https://api.nasa.gov/planetary/apod?api_key=xAP1HKs1wN8evwCa3vQjye47l8WBGVtcM5UFtTT8&count=1",
+        url: "https://api.nasa.gov/planetary/apod?api_key=xAP1HKs1wN8evwCa3vQjye47l8WBGVtcM5UFtTT8",
         type: "GET",
         dataType: "json",
         success: function(data) {
-           let copyRight =  data.copyright;
            let date = data.date;
            let explanation = data.explanation;
-           let media_type = data.media_type;
            let title = data.title;
            let url = data.url;
            $('#title').text(title)
@@ -34,3 +26,22 @@ function fetchNasa(){
     })
 }
 
+function fetchNasa(){
+
+    $.ajax({
+        url: "https://api.nasa.gov/planetary/apod?api_key=xAP1HKs1wN8evwCa3vQjye47l8WBGVtcM5UFtTT8&count=1",
+        type: "GET",
+        dataType: "json",
+        success: function(data) {
+           let date = data[0].date;
+           let explanation = data[0].explanation;
+           let title = data[0].title;
+           let url = data[0].url;
+           $('#title').text(title)
+           $("#date").text(date)
+           $("#explanation").text(explanation)
+           $("#pic").attr("src", url)
+        }
+
+    })
+}
