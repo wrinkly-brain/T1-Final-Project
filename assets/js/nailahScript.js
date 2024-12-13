c9321928a9851340de270e4a8b984c8ee196ebc5
Nailah <nailah.markham@atlasschool.com>
2024-12-13 03:51:46 -0600

    making sure everything is good on my end

--- assets/js/nailahScript.js
+++ assets/js/nailahScript.js
@@ -1,38 +1,4 @@
-// window.onload = getCertainStuffFromAPI();
 
-// async function getCertainStuffFromAPI() {
-//     try {
-//         const response = await axios.get("https://api.nasa.gov/planetary/apod?api_key=xAP1HKs1wN8evwCa3vQjye47l8WBGVtcM5UFtTT8&date=2024-12-12");
-
-//         console.log(response);
-
-//         console.log(response.copywrite);
-//     } catch (error) {
-//         console.log(error);
-//     }
-// }
-
-
-// // Specify the API endpoint for user data
-// const apiUrl = 'https://api.nasa.gov/planetary/apod?api_key=xAP1HKs1wN8evwCa3vQjye47l8WBGVtcM5UFtTT8&date=2024-12-12';
-
-// // Make a GET request using the Fetch API
-// fetch(apiUrl)
-//   .then(response => {
-//     if (!response.ok) {
-//       throw new Error('Network response was not ok');
-//     }
-//     return response.json();
-//   })
-//   .then(userData => {
-//     // Process the retrieved user data
-//     console.log('User Data:', userData);
-//   })
-//   .catch(error => {
-//     console.error('Error:', error);
-//   });
-
- 
 $(document).ready(function (){
     fetchNasa()
 });
@@ -58,3 +24,4 @@ function fetchNasa(){
 
     })
 }
+
