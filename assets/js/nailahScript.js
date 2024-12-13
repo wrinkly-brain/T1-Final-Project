6fbf33746c8dcd8d63dae515410a04534a5ecc23
Nailah <nailah.markham@atlasschool.com>
2024-12-12 19:15:09 -0600

    figured out API Key

--- assets/js/nailahScript.js
+++ assets/js/nailahScript.js
@@ -1,12 +1,4 @@
-import axios from "axios";
+fetch("https://api.nasa.gov/planetary/apod?api_key=xAP1HKs1wN8evwCa3vQjye47l8WBGVtcM5UFtTT8")
+    .then(response => console.log(response))
+    .catch(error => console.error(error));
 
-axios
-    .get(
-           "https://api.nasa.gov/planetary/apod"
-    )
-    .then(function (response) {
-        console.log(response.data);
-    })
-    .catch(function (error) {
-        console.log(error);
-    });
